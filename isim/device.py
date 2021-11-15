44a39bd93c2d63f0a7046bf96e987fd5fd5ea2ae
archont94 <radoslaw.karasinski@gmail.com>
2021-11-15 17:37:17 +0100

    Change return type.

--- isim/device.py
+++ isim/device.py
@@ -287,12 +287,12 @@ class Device(SimulatorControlBase):
 
         self._run_command(f"io {self.udid} screenshot {shlex.quote(output_path)}")
 
-    def spawn(self, executable: str) -> None:
+    def spawn(self, executable: str) -> str:
         """Spawn a process by executing a given executable on a device."""
         command = 'spawn "%s" %s' % (self.udid, executable)
         return self._run_command(command)
 
-    def launch(self, identifier: str) -> None:
+    def launch(self, identifier: str) -> str:
         """Launch an application by identifier on a device."""
         command = 'launch "%s" "%s"' % (self.udid, identifier)
         return self._run_command(command)
