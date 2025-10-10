a78cfd2d3ad768fc45371a11d56beeacb5889547
Dale Myers <dalemyers@microsoft.com>
2025-10-10 12:11:21 +0100

    Fix typo in test method name
    Rename test_installed_devicess to test_installed_devices (remove extra 's').

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -75,7 +75,7 @@ class TestDevice(unittest.TestCase):
         finally:
             device.delete()
 
-    def test_installed_devicess(self):
+    def test_installed_devices(self):
         """Test that we can parse all installed devices without error."""
         self.assertIsNotNone(isim.Device.list_all())
 
